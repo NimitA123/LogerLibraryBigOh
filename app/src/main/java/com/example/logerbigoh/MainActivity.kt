748aa1163239efeaadc378548167c161163733b5
Nimit Agarwal <nimit.agrawal0105@gmail.com>
2022-10-11 10:46:41 +0530

    perf(): README added

--- app/src/main/java/com/example/logerbigoh/MainActivity.kt
+++ app/src/main/java/com/example/logerbigoh/MainActivity.kt
@@ -2,12 +2,13 @@ package com.example.logerbigoh
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.logerlibrary.MyPersonalApp
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        var myPersonalApp = MyPersonalApp()
-        myPersonalApp.onCreate()
+      /*  var myPersonalApp = MyPersonalApp()
+        myPersonalApp.onCreate()*/
     }
 }
--- logerLibrary/src/main/java/com/example/logerlibrary/MyPersonalApp.kt
+++ logerLibrary/src/main/java/com/example/logerlibrary/MyPersonalApp.kt
@@ -2,8 +2,10 @@ package com.example.logerlibrary
 
 import android.app.Application
 import android.os.Environment
+import android.util.Log
 import java.io.File
 import java.io.IOException
+import java.util.concurrent.TimeUnit
 
 
 class MyPersonalApp : Application() {
@@ -13,20 +15,36 @@ class MyPersonalApp : Application() {
     override fun onCreate() {
         super.onCreate()
 
+        var logDirectoryCreationTimeInMilli:Long = 0
+        var logDirectoryCreationTimeInMinute:Long = 0
+
+
+        //TODO This Current time in the milliSecond
+        var currentTime = System.currentTimeMillis()
+        //TODO this current time in the Minute.
+        var currentMinute = TimeUnit.MILLISECONDS.toMinutes(currentTime)
+
         if (isExternalStorageWritable) {
-            val appDirectory =
-                File(Environment.getExternalStorageDirectory().toString() + "/MyPersonalAppFolder")
-            val logDirectory = File("$appDirectory/logs")
+            val logDirectory = this.baseContext.filesDir.absolutePath + File.separator + "newfoldername"
             val logFile = File(logDirectory, "logcat_" + System.currentTimeMillis() + ".txt")
 
-            // create app folder
-            if (!appDirectory.exists()) {
-                appDirectory.mkdir()
-            }
 
             // create log folder
-            if (!logDirectory.exists()) {
-                logDirectory.mkdir()
+            if (!logFile.exists()) {
+                logFile.mkdir()
+                //TODO The file creation time in the milliSecond
+                logDirectoryCreationTimeInMilli = System.currentTimeMillis().toLong()
+                // TODO Convert milliSecond in minute.
+                logDirectoryCreationTimeInMinute = TimeUnit.MILLISECONDS.toMinutes(logDirectoryCreationTimeInMilli)
+
+
+                Log.d("nimit", "onCreate: ${System.currentTimeMillis()}")
+            }
+            // TODO file creation time is more than two days than delete file
+            if(logFile.exists()){
+                if(currentMinute - logDirectoryCreationTimeInMinute >= 2880){
+                    logFile.delete()
+                }
             }
 
             // clear the previous logcat and then write the new one to the file
