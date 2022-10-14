d574e7033e433eee66fe2f3acd12ecaa87dd82b9
Mohammed Ali <mohammad.ali@bigohtech.com>
2022-10-14 16:00:28 +0530

    perf(): version increase....

--- app/src/main/java/com/example/logerbigoh/MainActivity.kt
+++ app/src/main/java/com/example/logerbigoh/MainActivity.kt
@@ -3,15 +3,21 @@ package com.example.logerbigoh
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.logerlibrary.MainActivity
+import android.util.Log
+import com.example.logerlibrary.LogerActivity
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var instances:MainActivity
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-      /*  var myPersonalApp = MyPersonalApp()
-        myPersonalApp.onCreate()*/
-        var intent = Intent(this,  MainActivity::class.java)
-        startActivity(intent)
+
+
+
+
+
+
+
+
     }
 }
